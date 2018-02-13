9bcb8b7fd32eb549ade976e1a35a8b87d2ffff67
Alexa Rarick <ararick@four51.com>
2018-02-13 16:50:26 -0600

    ngForms work

---
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+    selector: 'passenger-form',
+    styleUrls: ['passenger-form.component.scss'],
+    template: `
+        <form #form="ngForm" novalidate>
+            {{detail | json}}
+            <div>
+                Passenger Name: 
+                <input 
+                    type="text"
+                    name="fullname"
+                    [ngModel]="detail?.fullname">
+            </div>
+            <div>
+                Passenger ID: 
+                <input 
+                    type="number"
+                    name="id"
+                    [ngModel]="detail?.id">
+            </div>
+
+
+            <div>
+                <label>
+                    <input type="radio"
+                           name="checkedIn"
+                           [value]="true"
+                           [ngModel]="detail?.checkedIn"
+                           (ngModelChange)="toggleCheckIn($event)">
+                        Yes
+                </label>
+                <label>
+                    <input type="radio"
+                           name="checkedIn"
+                           [value]="false"
+                           [ngModel]="detail?.checkedIn"
+                           (ngModelChange)="toggleCheckIn($event)">
+                        No
+            </label>
+            </div>
+
+            <div *ngIf="form.value.checkedIn">
+                Check In Date:
+                <input
+                    type="number"
+                    name="checkInDate"
+                    [ngModel]="detail?.checkInDate">
+            </div>
+            {{form.value | json}}
+        </form>
+    `
+})
+
+export class PassengerFormComponent {
+    @Input()
+    detail: Passenger;
+    toggleCheckIn(checkedIn: boolean) {
+        if(checkedIn) {
+            this.detail.checkInDate = +new Date();
+        }
+    }
+}
