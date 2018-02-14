8e3820f3d6168da35ab0f0ab20e5334db2fa025f
Alexa Rarick <ararick@four51.com>
2018-02-14 13:41:50 -0600

    form submission

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,46 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
+import { EventEmitter } from '@angular/forms/src/facade/async';
 
 @Component({
     selector: 'passenger-form',
     styleUrls: ['passenger-form.component.scss'],
     template: `
-        <form #form="ngForm" novalidate>
-            {{detail | json}}
+        <form #form="ngForm" (ngSubmit)="handleSubmit(form.value, form.valid)" novalidate>
             <div>
                 Passenger Name: 
                 <input 
                     type="text"
                     name="fullname"
-                    [ngModel]="detail?.fullname">
+                    #fullname="ngModel"
+                    [ngModel]="detail?.fullname"
+                    required>
             </div>
             <div>
                 Passenger ID: 
                 <input 
                     type="number"
                     name="id"
-                    [ngModel]="detail?.id">
+                    required
+                    #id="ngModel"
+                    [ngModel]="detail?.id">                
             </div>
 
 
             <div>
                 <label>
-                    <input type="radio"
+                    <input type="checkbox"
                            name="checkedIn"
-                           [value]="true"
                            [ngModel]="detail?.checkedIn"
                            (ngModelChange)="toggleCheckIn($event)">
-                        Yes
                 </label>
-                <label>
-                    <input type="radio"
-                           name="checkedIn"
-                           [value]="false"
-                           [ngModel]="detail?.checkedIn"
-                           (ngModelChange)="toggleCheckIn($event)">
-                        No
-            </label>
             </div>
 
             <div *ngIf="form.value.checkedIn">
@@ -50,7 +45,17 @@ import { Passenger } from '../../models/passenger.interface';
                     name="checkInDate"
                     [ngModel]="detail?.checkInDate">
             </div>
-            {{form.value | json}}
+            <div>
+                Luggage:
+                <select name="baggage"
+                        [ngModel]="detail?.baggage">
+                    <option *ngFor="let item of baggage" [value]="item.key" [selected]="item.key === detail?.baggage">
+                        {{item.value}}
+                    </option>
+                </select>
+
+            </div>
+            <button type="submit" [disabled]="form.invalid">Update Passenger</button>
         </form>
     `
 })
@@ -58,9 +63,33 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerFormComponent {
     @Input()
     detail: Passenger;
+
+    @Output()
+    update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+    
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No Baggage'
+    },{
+        key: 'hand-only',
+        value: 'Hand Baggage'
+    },{
+        key: 'hold-only',
+        value: 'Hold Baggage'
+    },{
+        key: 'hand-and-hold',
+        value: 'Hand and Hold Baggage'
+    }];
+
     toggleCheckIn(checkedIn: boolean) {
         if(checkedIn) {
             this.detail.checkInDate = +new Date();
+        }
+    }
+
+    handleSubmit(passenger: Passenger, valid: boolean) {
+        if (valid) {
+            this.update.emit(passenger);
         }
     }
 }
